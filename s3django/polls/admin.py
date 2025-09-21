dbbde7e1ded14356dc4ac3b5e19223a5203116d8
pjsulley <thepatrickjohnsullivan@gmail.com>
2025-09-21 22:51:48 +1200

    Django tutorial part 7

--- s3django/polls/admin.py
+++ s3django/polls/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 
-from .models import Question
-from .models import Choice
+from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+    search_fields = ["question_text"]
+    list_filter = ["pub_date"]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+
+
+admin.site.register(Question, QuestionAdmin)
